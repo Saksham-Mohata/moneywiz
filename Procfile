b71d8c75cbf216b603121311351c4be9477e319d
Saksham-Mohata <sakshammohata@gmail.com>
2023-07-07 14:51:44 +0530

    first commit

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn registration.wsgi
--- registration/settings.py
+++ registration/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-hi9u%zpi%!_33p0(6kg!z=c&bdcc)%+mgho22w4o#zsw_psmr)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app' , '.now.sh']
 
 
 # Application definition
@@ -117,12 +117,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_ROOT = 'staticfiles'
+
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles')
-STATICFILES_DIR = {
-    os.path.join(BASE_DIR , 'public/static')
-}
+STATICFILES_DIR = os.path.join(BASE_DIR , 'static')
+
+STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles_build', 'static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
