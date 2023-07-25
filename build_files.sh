177d3764b12b15df1e71b49b80abd2423ca467e7
Saksham-Mohata <sakshammohata@gmail.com>
2023-07-25 20:33:04 +0530

    first

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
 echo "BUILD START"
-C:\users\hp\appdata\local\programs\python\python38\python.exe -m pip install -r requirements.txt
-C:\users\hp\appdata\local\programs\python\python38\python.exe manage.py collectstatic  --noinput --clear 
+python3.8 -m pip install -r requirements.txt
+python3.8 manage.py collectstatic  --noinput --clear 
 echo "BUILD END"
